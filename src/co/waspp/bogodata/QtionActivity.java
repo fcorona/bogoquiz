1ec8aed9231a5d50c44c585d63be075c4208dcc1
bogoquiz <contactsaliens@gmail.com>
2014-03-29 16:48:35 -0500

    version 1
    perfomance de la prsentacion

--- src/co/waspp/bogodata/QtionActivity.java
+++ src/co/waspp/bogodata/QtionActivity.java
@@ -65,9 +65,12 @@ public class QtionActivity extends ActionBarActivity {
 		
 		//Medio Ambiemte
 		case 2:
+			handler.execute("http://api.bogotacomovamos.org/api/datas/120/?key=comovamos/");
+			break;
+		case 3:
 		default:
 			
-			handler.execute("http://api.bogotacomovamos.org/api/datas/120/?key=comovamos/");
+			handler.execute("hhttp://api.bogotacomovamos.org/api/datas/82/?key=comovamos/");
 			break;
 		}
 		
@@ -96,13 +99,17 @@ public class QtionActivity extends ActionBarActivity {
 				selectEducation(data);
 				break;
 			case 1:
-				//selectMovility(data);
+				selectMovility(data);
 				break;
 			
 			//Medio Ambiente
 			case 2:
+				selectGreen(data);
+				break;
+				
+			case 5:
 			default:
-				selectGreen(data);			
+				selectSocial(data);			
 				
 				break;
 			}
@@ -157,7 +164,7 @@ public class QtionActivity extends ActionBarActivity {
 	}
 	
 	public void selectEducation(JSONObject data){
-		//Para Arboles por cada 100.000 hab
+		//Para analfabetismo
 		int year = Calendar.getInstance().get(Calendar.YEAR);
 		
 		String fdata = null;
@@ -192,7 +199,108 @@ public class QtionActivity extends ActionBarActivity {
 				done=false;
 				num[i] = this.rand.nextInt(3);
 				for(int j=0;j<i;j++){
-					Toast.makeText(this, num[j]+","+num[i], Toast.LENGTH_SHORT).show();
+					if(num[j]==num[i]){
+						done=true;
+					}
+				}
+				
+			}while(done);
+			
+			Random rand = new Random();
+				
+			int r2 = maxYear - rand.nextInt(10) -5;
+			
+			//Log.d("Data", "n: "+num[i]+", data: "+r2);
+			this.answer[num[i]].setText(Integer.toString(r2));
+			
+		}
+	}
+	
+	public void selectMovility(JSONObject data){
+		//
+		int year = Calendar.getInstance().get(Calendar.YEAR);
+		
+		String fdata = null;
+		//HashMap<String, String> answer = new HashMap<String, String>();
+		
+		do{
+			try {
+				fdata = data.getString(Integer.toString(year));
+				//answer.put(Integer.toString(year), fdata);
+			} catch (JSONException e) { e.printStackTrace();}
+			  catch (NumberFormatException e) { e.printStackTrace();}
+			finally {
+				year--;
+			}
+		}while(year>=1998 && fdata==null);// && answer.size()<1);
+		
+		question.setText("¿Que porcentaje de pasajeros de Transporte publico se movilizados en Transmilenio?  ");
+		
+		//answers
+		this.answer[num[0]].setText( Float.toString(Float.parseFloat(fdata)*100)+"%" );
+		
+		for(int i=1;i<this.answer.length;i++){
+			boolean done;
+			
+			do{
+				done=false;
+				num[i] = this.rand.nextInt(3);
+				for(int j=0;j<i;j++){
+					if(num[j]==num[i]){
+						done=true;
+					}
+				}
+				
+			}while(done);
+			
+			Random rand = new Random();
+			
+			
+			float m = Float.parseFloat(fdata)*100;
+			float r2 = m - rand.nextInt(10) - 5;
+			
+			//Log.d("Data", "n: "+num[i]+", data: "+r2);
+			this.answer[num[i]].setText(Float.toString(r2)+"%");
+			
+		}
+	}
+	
+	public void selectSocial(JSONObject data){
+		//hurto
+		int year = 2010;//Calendar.getInstance().get(Calendar.YEAR);
+		
+		String fdata = null;
+		//HashMap<String, String> answer = new HashMap<String, String>();
+		
+		float max = 0;
+		int maxYear = year;
+		
+		do{
+			try {
+				fdata = data.getString(Integer.toString(year));
+				if(Integer.parseInt(fdata)>max){
+					maxYear = year;
+				}
+				//answer.put(Integer.toString(year), fdata);
+			} catch (Exception e) { e.printStackTrace();}
+			  //catch (NumberFormatException e) { e.printStackTrace();}
+			finally {
+				year--;
+			}
+		}while(year>=1998);// && answer.size()<1);
+		
+		question.setText("¿En qué año se presentó el mayor hurto a personas? ");
+		
+		//answers
+		this.answer[num[0]].setText( Integer.toString(maxYear));
+		
+		for(int i=1;i<this.answer.length;i++){
+			boolean done;
+			
+			do{
+				done=false;
+				num[i] = this.rand.nextInt(3);
+				for(int j=0;j<i;j++){
 					if(num[j]==num[i]){
 						done=true;
 					}
@@ -227,7 +335,7 @@ public class QtionActivity extends ActionBarActivity {
 			}
 			
 			Toast.makeText(this, mess, Toast.LENGTH_SHORT).show();
-			
+			 
 			try {
 				Thread.sleep(2000);
 			} catch (InterruptedException e) {e.printStackTrace();}
